b141a31e01986533e96d49c2bce8617512a261ef
Buran436 <catplanet436@gmail.com>
2025-01-23 22:17:19 +0300

    added animations. improved mobile version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,4 +7,13 @@ function sectionLoadAnimate() {
             section.classList.add('visible');
         }, 200 * (i + 1));
     });
+
+    setTimeout(() => {
+        const sections = document.querySelectorAll('.links__link');
+        sections.forEach((section, i) => {
+            setTimeout(() => {
+                section.classList.add('visible');
+            }, 150 * (i + 1));
+        });
+    }, 600)
 }
